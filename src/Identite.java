2f26632ec7a04f2fe4c8c8cfd919d746cc5172e3
Quentin Donadoni <quentin.donadoni@gmail.com>
2020-09-23 15:45:25 +0200

    Création classe identité

---
+++ src/Identite.java
@@ -0,0 +1,28 @@
+
+public class Identite {
+	
+	private String NIP,nom,prenom;
+
+	/**
+	 * @return the nIP
+	 */
+	public String getNIP() {
+		return NIP;
+	}
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+	
+	
+}
