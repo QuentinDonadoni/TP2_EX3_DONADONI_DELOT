4c735ce6d078c35f43bda258134a8a9cd50d6ab1
LuckifirE <58765636+LuckifirE@users.noreply.github.com>
2020-09-29 10:42:51 +0200

    isMat

--- src/Formation.java
+++ src/Formation.java
@@ -1,4 +1,15 @@
+import java.util.HashMap;
 
 public class Formation {
 
+	private String identifiant;
+	private HashMap<String,Double> listMat;
+	
+	public Formation(String id,) {
+		
+	}
+	
+	public boolean isMat(String mat) {
+		
+	}
 }
